763bf01967cf1be1fdf57adbd5d0ec2ed46bed36
Valentin Loya <a.22308051281244@cbtis128.edu.mx>
2024-09-13 10:17:58 -0600

    Add files via upload

---
+++ funcionesv2.py
@@ -0,0 +1,37 @@
+print("Funciones version 2")
+print("Valentin Loya")
+# Zona de listas tuplas set y diccionario
+celulares=["Samsung A52" , "iPhone 15" , "Chafa"]
+carros=["Charger" , "Challenger" , "Camaro"]
+marcas=["Adidas" , "Nike" , "Puma"]
+nombres=["Ana" , "Derek" , "Chona"]
+# Zona de funciones
+# Lista
+def verlista(telefonos):
+    for uncelular in telefonos:
+        print(uncelular)
+# Conjunto
+def vconjunto(autos):
+    for unauto in autos:
+        print(unauto)
+# Tupla
+def vtupla(tenis):
+    for unteni in tenis:
+        print(unteni)
+# Diccionario       
+def vDicc(misnovias):
+    for unanovia in misnovias:
+        print(unanovia)
+# Llamadas a funciones 
+# Llamada a lista
+print("\nImprime celulares de una lista")
+verlista(celulares)
+# Llamada a conjunto
+print("\nImprime carros de un conjunto")
+vconjunto(carros)
+# Llamada a tupla
+print("\nImprime marcas de tenis de una tupla")
+vtupla(marcas)
+# Llamada a diccionario
+print("\nImprime los nombres de mis novias de un diccionario")
+vDicc(nombres)
